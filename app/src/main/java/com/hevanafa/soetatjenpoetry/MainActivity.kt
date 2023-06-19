1127e9ab5728a024da97767d0a2a7d5d00560633
Hevanafa <dhevansafa4@gmail.com>
2023-06-19 14:42:46 +0700

    Removed unused imports

--- app/src/main/java/com/hevanafa/soetatjenpoetry/MainActivity.kt
+++ app/src/main/java/com/hevanafa/soetatjenpoetry/MainActivity.kt
@@ -1,54 +1,21 @@
 package com.hevanafa.soetatjenpoetry
 
 import android.os.Bundle
-import android.util.Log
 import androidx.activity.ComponentActivity
-import androidx.activity.OnBackPressedCallback
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.Image
-import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.absolutePadding
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.lazy.grid.GridCells
-import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
-import androidx.compose.foundation.lazy.grid.items
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.ArrowBack
-import androidx.compose.material3.Card
-import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.Icon
-import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
-import androidx.compose.runtime.remember
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.StrokeJoin
-import androidx.compose.ui.graphics.asImageBitmap
-import androidx.compose.ui.graphics.drawscope.Stroke
-import androidx.compose.ui.layout.ContentScale
-import androidx.compose.ui.text.ExperimentalTextApi
-import androidx.compose.ui.text.TextStyle
-import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
 import androidx.lifecycle.viewmodel.compose.viewModel
-import androidx.navigation.NavController
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
@@ -106,12 +73,10 @@ class MainActivity : ComponentActivity() {
         viewModel!!.loadPoems(applicationContext)
 
         navController = rememberNavController()
-        val backStackEntry by navController!!.currentBackStackEntryAsState()
-        val currentRoute = getCurrentRoute()
+        // val backStackEntry by navController!!.currentBackStackEntryAsState()
+        // val currentRoute = getCurrentRoute()
 
         // navController.addOnDestinationChangedListener(NavController.OnDestinationChangedListener(navController, backStackEntry.first))
-
-//        val activePoem: Poem? = viewModel!!.getActivePoem()
 
         Scaffold (topBar = {
             MainTopBar(
--- app/src/main/java/com/hevanafa/soetatjenpoetry/StateViewModel.kt
+++ app/src/main/java/com/hevanafa/soetatjenpoetry/StateViewModel.kt
@@ -6,10 +6,8 @@ import android.graphics.BitmapFactory
 import android.util.Base64
 import android.util.Log
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateListOf
 import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.setValue
 import androidx.lifecycle.ViewModel
 import com.google.gson.Gson
 import java.io.IOException
